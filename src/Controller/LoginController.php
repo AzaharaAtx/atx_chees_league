82aac3e02481423eae00ff5e28894f0acd17445d
Azahara <azahara22lopez@gmail.com>
2023-11-22 13:47:10 +0100

    commit

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,7 +6,7 @@ use App\Entity\User;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
@@ -16,14 +16,27 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login_chess', name: 'api_app_login'/*,  methods: ['POST']*/)]
-    public function index(): string
-    {
-        //$user = $doctrine->getRepository(User::class)->findAll();
-        //$find = json_decode($user);
 
-       // return $this->json($user);
-        return "Hola";
+    #[Route('/login_chess', name: 'api_app_login')]
+    public function index(ManagerRegistry $doctrine): JsonResponse
+    {
+        //$userFake = ['full_name' => "hola", "password" => '123'];
+        $user = $doctrine->getRepository(User::class)->findAll();
+        $userData = [];
+
+        foreach ($user as $datauser) {
+            $userData[] = [
+                'id' => $datauser->getId(),
+                'full_name' => $datauser->getFullName(),
+                'email' => $datauser->getEmail(),
+                'password' => $datauser->getPassword(),
+                'roles'  => $datauser->getRoles(),
+            ];
+        }
+
+
+        return $this->json($userData);
+        //dump($user);
 
     }
 
