11a49fa66ff33f167f5d04c07c390ca589e37d44
Azahara <azahara22lopez@gmail.com>
2023-11-23 12:13:52 +0100

    commit

--- src/Controller/UserRegisterController.php
+++ src/Controller/UserRegisterController.php
@@ -2,18 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserRegisterController extends AbstractController
 {
-    #[Route('/user/register', name: 'app_user_register')]
-    public function index(): JsonResponse
+    private UserPasswordHasherInterface $userPasswordHasher;
+
+    //Inyectamos la interfaaz para hashear la contraseña
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserRegisterController.php',
-        ]);
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+    #[Route('/user/register', name: 'app_user_register')]
+    public function create(Request $request, ManagerRegistry $doctrine): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        //Verificar si el usuario ya existe
+        $userExist = $doctrine->getRepository(User::class)->findOneBy(['email' => $data['email']]);
+
+        if ($userExist) {
+            return new JsonResponse(['error' => 'El usuario con este correo electrónico ya existe'], Response::HTTP_CONFLICT);
+        }
+
+        //Crear una nueva instancia de User
+        $user = new User();
+        $user->setEmail($data['email']);
+        $user->setFullName($data['full_name']);
+        $user->setLastName($data['last_name']);
+        $user->setUserPlayer($data['user_player']);
+        $user->setUserRole($data['roles']);
+
+        //Crear y hashear la contraseña antes de almacenarla
+        $hash = $this->userPasswordHasher->hashPassword(
+            $user,
+            $data['password']
+        );
+        $user->setPassword($hash);
+
+        //Guardamos el nuevo usuario
+        $em = $doctrine->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return new JsonResponse(['message' => 'Usuario creado con éxito'.$user->getUser()], Response::HTTP_CREATED);
     }
 }
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -3,8 +3,11 @@
 namespace App\Repository;
 
 use App\Entity\User;
+use App\Queries\UserQuery\UserQuery;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\DBAL\Exception;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
@@ -26,20 +29,45 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
     }
 
     /**
+     * Actualiza la nueva contraseña hasheada para el usuario
      * Used to upgrade (rehash) the user's password automatically over time.
      */
-
     public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
     {
+        // Verifica si la interfaz PasswordAuthenticatedUserInterface está implementada por el usuario.
         if (!$user instanceof User) {
+            // Si el usuario no es una instancia de la clase User, lanza una excepción UnsupportedUserException.
             throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', $user::class));
         }
-
+        // Establece la nueva contraseña hasheada para el usuario.
         $user->setPassword($newHashedPassword);
+        // Obtiene el EntityManager y persiste los cambios en el usuario.
         $this->getEntityManager()->persist($user);
+        // Realiza el flush para sincronizar los cambios en la base de datos.
         $this->getEntityManager()->flush();
     }
 
+    /**
+     * @throws Exception
+     * conditions, ordenations, limits -> marcador/localizador front
+     */
+    public function selection($query, $condition, $ordenation, $limit)
+    {
+        // Tratar SQL y preparacion
+        $sql = $query->getQuery();
+        $sql = str_replace('{{conditions}}', $condition, $sql);
+        $sql = str_replace('{{ordenations}}', $ordenation, $sql);
+        $sql = str_replace('{{limits}}', $limit, $sql);
+        $connection = $this->getEntityManager()->getConnection()->prepare($sql);
+        // Ejecuto
+        $connectionResult = $connection->executeQuery();
+        // Obtener información
+        $result = $connectionResult->fetchAllAssociative();
+
+        return $result;
+    }
+
+    // Metodo para listar usuarios por id
     public function findUser($id)
     {
         return $this->getEntityManager()
@@ -50,23 +78,42 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
             )
             ->setParameter('id', $id)
             ->getResult();
+    }
+
+    // Método listado completo
+    public function findAllUser()
+    {
+        //condiciones
+        $condition  = '';
+        //ordenaciones
+        $ordenation = ' ORDER BY u.id DESC ';
+        //limites
+        $limit    = '';
+
+        $query = new UserQuery();
+        $result = $this->selection($query, $condition, $ordenation, $limit);
+
+        return $result;
 
     }
 
-    /**
-     * @return User[] Returns an array of User objects
-     */
-   /* public function findByExampleField($value): array
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('u.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }*/
+
+//
+//    /**
+//     * @return User[] Returns an array of User objects
+//     */
+//
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('u')
+//            ->andWhere('u.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('u.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
 
 //    public function findOneBySomeField($value): ?User
 //    {
